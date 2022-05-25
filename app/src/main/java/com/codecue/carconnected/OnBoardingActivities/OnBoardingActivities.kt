1f1ef94e83375b0a2566f24ecab019f86029426c
BasitMughal <basimughal7583@gmail,com>
2022-05-25 14:56:12 +0500

    theme

---
+++ app/src/main/java/com/codecue/carconnected/OnBoardingActivities/OnBoardingActivities.kt
@@ -0,0 +1,12 @@
+package com.codecue.carconnected.OnBoardingActivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codecue.carconnected.R
+
+class OnBoardingActivities : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_activities)
+    }
+}
